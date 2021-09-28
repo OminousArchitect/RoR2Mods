f1c52444fe0a55e80d130fb2d87993a775e6ef6e
Bubbet <brett1057228@gmail.com>
2021-09-27 19:18:20 -0600

    better NightHunter.cs behaviour

--- VayneMod/SkillStates/NightHunter.cs
+++ VayneMod/SkillStates/NightHunter.cs
@@ -13,15 +13,20 @@ namespace VayneMod
         private float radius = 5f;
         private float searchrate = 5f;
         private int speedincrease = 2;
-        
+
+        private BullseyeSearch search;
+        private float oldDistance;
+        private int changeInDirection;
+
         public void Awake()
         {
             body = GetComponent<CharacterBody>();
-        }
-
-        public void NightHunterBehaviour()
-        {
-            BullseyeSearch bullseyeSearch = new BullseyeSearch
+            RecalculateStatsAPI.GetStatCoefficients += (sender, args) =>
+            {
+                args.baseMoveSpeedAdd += sender.GetComponent<NightHunter>()?.speedincrease ?? 0f;
+            };
+            
+            search = new BullseyeSearch
             {
                 
                 teamMaskFilter = TeamMask.GetEnemyTeams(TeamIndex.Monster),
@@ -31,26 +36,25 @@ namespace VayneMod
                 filterByLoS = false
                 
             };
-            bullseyeSearch.RefreshCandidates();
-            bullseyeSearch.FilterOutGameObject(Prefabs.vayneprefab);
-
-            HurtBox beingHunted = bullseyeSearch.GetResults().First<HurtBox>();
-
-            if (beingHunted)
-            {
-                RecalculateStatsAPI.GetStatCoefficients += (sender, args) =>
-                {
-                    args.baseMoveSpeedAdd += sender.GetComponent<NightHunter>()?.speedincrease ?? 0f;
-                };
-            }
+            search.FilterOutGameObject(Prefabs.vayneprefab);
+            search.RefreshCandidates();
         }
-        
+
         public void FixedUpdate()
         {
             stopwatch += Time.fixedDeltaTime;
             if (stopwatch >= 1f / searchrate)
             {
-                NightHunterBehaviour();
+                search.RefreshCandidates();
+                using (var results = search.GetResults().GetEnumerator())
+                {
+                    if (results.Current != null)
+                    {
+                        var currentDist = Vector3.Distance(body.corePosition, results.Current.transform.position);
+                        changeInDirection = Mathf.RoundToInt(Mathf.Clamp(oldDistance - currentDist, -1, 1)); // Might need to swap the substraction around here
+                        oldDistance = currentDist;
+                    }
+                }
             }
         }
     }
