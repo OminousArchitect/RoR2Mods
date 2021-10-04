21cbee6c49ebdf0871201e18ed673c80fb2e7aff
Bubbet <brett1057228@gmail.com>
2021-10-03 22:17:38 -0600

    better NightHunter.cs behaviour round 3

--- VayneMod/SkillStates/NightHunter.cs
+++ VayneMod/SkillStates/NightHunter.cs
@@ -10,76 +10,84 @@ namespace VayneMod
     public class NightHunter : MonoBehaviour
     {
         // Filled via editor 
-        public float speedincrease = 2f;
-        public float radius = 30f;
-        public float searchrate = 5f;
-        private float _stopwatch;
-        private float _oldDistance;
-        public CharacterBody _body;
-        private BullseyeSearch _search;
-        public CharacterMotor _motor;
+        public CharacterBody body;
+        public CharacterMotor characterMotor;
+        public float speedincrease = 5f;
+        public float radius = 80f;
+        public float searchrate = 15f;
         
-        public Distance changeInDirection;
+        private float _stopwatch;
+        private BullseyeSearch _search;
+        private float _oldDistance;
+        
+        public Direction changeInDirection;
 
         public void Awake()
         {
+            //characterMotor = GetComponent<CharacterMotor>();
+            //body = GetComponent<CharacterBody>();
+            
             _search = new BullseyeSearch
             {
-                teamMaskFilter = TeamMask.GetEnemyTeams(TeamIndex.Monster),
+                teamMaskFilter = TeamMask.GetUnprotectedTeams(body.teamComponent.teamIndex),
                 maxDistanceFilter = radius,
                 sortMode = BullseyeSearch.SortMode.Distance,
                 filterByLoS = false,
-                maxAngleFilter = 45f
+                maxAngleFilter = 30f
             };
-
-            _body = GetComponent<CharacterBody>();
-            _motor = GetComponent<CharacterMotor>();
         }
 
         private HurtBox DoSearch()
         {
             _search.searchOrigin = transform.position;
-            _search.searchDirection = _motor.velocity.normalized;
+            _search.searchDirection = characterMotor.velocity.normalized;
             _search.RefreshCandidates();
-            _search.FilterOutGameObject(Prefabs.vayneprefab); // This needs to come after candidates are refreshed
+            _search.FilterOutGameObject(gameObject); // This needs to come after candidates are refreshed
             
+            Debug.Log("doing search");
             /* instead of using you could do this
-            var doSearch = DoSearch();
-            var results = doSearch.Current;
-            doSearch.Dispose();
+            var test = DoSearch();
+            var test2 = test.Current;
+            test.Dispose();
             */
-            using (var results = _search.GetResults().GetEnumerator()) // You have to dispose of enumerables when you're done with them using automatically does this.
+            return _search.GetResults().FirstOrDefault();
+            //using (var results = _search.GetResults().GetEnumerator()) // You have to dispose of enumerables when you're done with them using automatically does this.
             {
-                //Debug.LogWarning($"{results.Current}");
-                return results.Current;
+                //return results.Current;
             }
         }
 
         public void FixedUpdate()
         {
             _stopwatch += Time.fixedDeltaTime;
+            //Debug.Log(_stopwatch + "/" + 1f / searchrate);
             if (_stopwatch >= 1f / searchrate)
             {
+                _stopwatch -= 1f / searchrate;
                 var hurtBox = DoSearch();
                 if (hurtBox)
                 {
+                    Debug.Log("found hurtbox");
                     var currentDist = Vector3.Distance(transform.position, hurtBox.healthComponent.transform.position);
-                    var deltaDist = currentDist - _oldDistance; // Might need to swap the substraction around here
+                    var deltaDist = _oldDistance - currentDist; // Might need to swap the substraction around here
                     if (deltaDist > 0.001f) // Most likely condition first
-                        changeInDirection = Distance.Closer; // moving towards
+                        changeInDirection = Direction.Closer; // moving towards
                     else if (deltaDist < -0.001f)
-                        changeInDirection = Distance.Further; // moving away
+                        changeInDirection = Direction.Further; // moving away
                     else
-                        changeInDirection = Distance.None; // no change
+                        changeInDirection = Direction.None; // no change
+                    Debug.Log(changeInDirection);
                     _oldDistance = currentDist;
                 }
+                else
+                {
+                    changeInDirection = Direction.None;
+                }
+                body.RecalculateStats();
             }
-            
-            var currentmovespeed = _body.moveSpeed;
-            Debug.Log($"{_search.GetResults().GetEnumerator().Current}" );
         }
 
-        public enum Distance
+        public enum Direction
         {
             None,
             Closer,
