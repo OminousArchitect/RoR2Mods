6c44f89139e443e847c42396a92f449597e88393
Nines <bosaust@gmail.com>
2021-10-10 00:01:08 -0400

    -Condemn changes

---
+++ VayneMod/SkillStates/Condemn.cs
@@ -0,0 +1,25 @@
+﻿using EntityStates;
+using UnityEngine;
+
+namespace VayneMod.SkillStates
+{
+    public class Condemn : GenericProjectileBaseState
+    {
+        public override void OnEnter()
+        {
+            baseDuration = 0.75f;
+            baseDelayBeforeFiringProjectile = 0.2f;
+            projectilePrefab = Assets.mainAssetBundle.LoadAsset<GameObject>("silverboltprefab"); 
+            damageCoefficient = 1.75f;
+            force = 15f;
+            targetMuzzle = "Muzzle";
+            base.OnEnter();
+        }
+
+        public override void PlayAnimation(float duration)
+        {
+            PlayAnimation("Gesture, Override", "ShootGun", "ShootGun.playbackRate", base.duration);
+            base.PlayAnimation(duration);
+        }
+    }
+}
